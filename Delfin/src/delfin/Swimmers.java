f717f24ad70bd67289939ca7ec6b4569648aa082
EmilNeis <44893984+EmilNeis@users.noreply.github.com>
2018-11-12 21:39:56 +0100

    Added the methods to get members sorted by info
    They might not work just yet but they are put in and can easily be made funktional

--- Delfin/src/delfin/Swimmers.java
+++ Delfin/src/delfin/Swimmers.java
@@ -5,6 +5,11 @@
  */
 package delfin;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -29,7 +34,12 @@ public class Swimmers {
         this.pro = pro;
         this.payment = payment;
     }
-
+    
+    private DBConnector connector = null;
+  
+     Swimmers(DBConnector connector) {
+        this.connector = connector;
+     }
     public String getName()
     {
         return name;
@@ -89,7 +99,265 @@ public class Swimmers {
     {
         this.payment = payment;
     }
+    public ArrayList<Swimmers> getSvimmersByName() {
+        //throw new UnsupportedOperationException("Not supported yet.");
+        try{
+        String query = "Select * from user Order By 'name';";
 
+        Connection connection = connector.getConnection();
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+        
+        String name = "";
+        //Date dob = 1998-01-01;
+        String email = "";
+        boolean status = true;
+        boolean pro = true;
+        boolean payment = true;
+        
+        
+        ArrayList<Swimmers> swimmers = new ArrayList();
+
+        while (rs.next()) {
+            
+            name = rs.getString("name");
+            dob = rs.getDate("dob");
+            email = rs.getString("email");
+            status = rs.getBoolean("status");
+            pro = rs.getBoolean("pro");
+            payment = rs.getBoolean("payment");
+            
+            swimmers.add(new Swimmers(name, dob, email, status, pro, payment));
+        
+        
+        }
+        return swimmers;
+        }
+        catch(SQLException ex){
+                
+                
+        }
+        return null;
+    }
+    
+    
+    
+    
+    public ArrayList<Swimmers> getSvimmersByDob() {
+        //throw new UnsupportedOperationException("Not supported yet.");
+        try{
+        String query = "Select * from user Order By 'dob';";
+
+        Connection connection = connector.getConnection();
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+        
+        String name = "";
+        //Date dob = 1998-01-01;
+        String email = "";
+        boolean status = true;
+        boolean pro = true;
+        boolean payment = true;
+        
+        
+        ArrayList<Swimmers> swimmers = new ArrayList();
+
+        while (rs.next()) {
+            
+            name = rs.getString("name");
+            dob = rs.getDate("dob");
+            email = rs.getString("email");
+            status = rs.getBoolean("status");
+            pro = rs.getBoolean("pro");
+            payment = rs.getBoolean("payment");
+            
+            swimmers.add(new Swimmers(name, dob, email, status, pro, payment));
+        
+        
+        }
+        return swimmers;
+        }
+        catch(SQLException ex){
+                
+                
+        }
+        return null;
+    }
+    
+    
+    
+    
+    public ArrayList<Swimmers> getSvimmersByEmail() {
+        //throw new UnsupportedOperationException("Not supported yet.");
+        try{
+        String query = "Select * from user Order By 'email';";
+
+        Connection connection = connector.getConnection();
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+        
+        String name = "";
+        //Date dob = 1998-01-01;
+        String email = "";
+        boolean status = true;
+        boolean pro = true;
+        boolean payment = true;
+        
+        
+        ArrayList<Swimmers> swimmers = new ArrayList();
+
+        while (rs.next()) {
+            
+            name = rs.getString("name");
+            dob = rs.getDate("dob");
+            email = rs.getString("email");
+            status = rs.getBoolean("status");
+            pro = rs.getBoolean("pro");
+            payment = rs.getBoolean("payment");
+            
+            swimmers.add(new Swimmers(name, dob, email, status, pro, payment));
+        
+        
+        }
+        return swimmers;
+        }
+        catch(SQLException ex){
+                
+                
+        }
+        return null;
+    }
+    
+    
+    
+    public ArrayList<Swimmers> getSvimmersByStatus() {
+        //throw new UnsupportedOperationException("Not supported yet.");
+        try{
+        String query = "Select * from user Order By 'status';";
+
+        Connection connection = connector.getConnection();
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+        
+        String name = "";
+        //Date dob = 1998-01-01;
+        String email = "";
+        boolean status = true;
+        boolean pro = true;
+        boolean payment = true;
+        
+        
+        ArrayList<Swimmers> swimmers = new ArrayList();
+
+        while (rs.next()) {
+            
+            name = rs.getString("name");
+            dob = rs.getDate("dob");
+            email = rs.getString("email");
+            status = rs.getBoolean("status");
+            pro = rs.getBoolean("pro");
+            payment = rs.getBoolean("payment");
+            
+            swimmers.add(new Swimmers(name, dob, email, status, pro, payment));
+        
+        
+        }
+        return swimmers;
+        }
+        catch(SQLException ex){
+                
+                
+        }
+        return null;
+    }
+    
+    
+    
+    
+    public ArrayList<Swimmers> getSvimmersByPro() {
+        //throw new UnsupportedOperationException("Not supported yet.");
+        try{
+        String query = "Select * from user Order By 'pro';";
+
+        Connection connection = connector.getConnection();
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+        
+        String name = "";
+        //Date dob = 1998-01-01;
+        String email = "";
+        boolean status = true;
+        boolean pro = true;
+        boolean payment = true;
+        
+        
+        ArrayList<Swimmers> swimmers = new ArrayList();
+
+        while (rs.next()) {
+            
+            name = rs.getString("name");
+            dob = rs.getDate("dob");
+            email = rs.getString("email");
+            status = rs.getBoolean("status");
+            pro = rs.getBoolean("pro");
+            payment = rs.getBoolean("payment");
+            
+            swimmers.add(new Swimmers(name, dob, email, status, pro, payment));
+        
+        
+        }
+        return swimmers;
+        }
+        catch(SQLException ex){
+                
+                
+        }
+        return null;
+    }
+    
+    
+    
+    
+    public ArrayList<Swimmers> getSvimmersByPayment() {
+        //throw new UnsupportedOperationException("Not supported yet.");
+        try{
+        String query = "Select * from user Order By 'payment';";
+
+        Connection connection = connector.getConnection();
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+        
+        String name = "";
+        //Date dob = 1998-01-01;
+        String email = "";
+        boolean status = true;
+        boolean pro = true;
+        boolean payment = true;
+        
+        
+        ArrayList<Swimmers> swimmers = new ArrayList();
+
+        while (rs.next()) {
+            
+            name = rs.getString("name");
+            dob = rs.getDate("dob");
+            email = rs.getString("email");
+            status = rs.getBoolean("status");
+            pro = rs.getBoolean("pro");
+            payment = rs.getBoolean("payment");
+            
+            swimmers.add(new Swimmers(name, dob, email, status, pro, payment));
+        
+        
+        }
+        return swimmers;
+        }
+        catch(SQLException ex){
+                
+                
+        }
+        return null;
+    }
     @Override
     public String toString() {
         return "Swimmers{" + "name=" + name + ", dob=" + dob + ", email=" + email + ", status=" + status + ", pro=" + pro + ", payment=" + payment + '}';
