74961bebf385d7f7918f4469724bcb800a0fd4f5
DitlevR <ditlev1997@hotmail.com>
2018-11-12 11:00:51 +0100

    Team CLass
    Team Class

--- Delfin/src/delfin/Team.java
+++ Delfin/src/delfin/Team.java
@@ -1,14 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package delfin;
 
-/**
- *
- * @author Rumle
- */
+import java.util.ArrayList;
+
 public class Team {
+    private String TeamID;
+    private String Name;
+    ArrayList<Swimmers> members = new ArrayList();
+
+    public Team(String team_id, String teamName, ArrayList<Swimmers> members) {
+        this.TeamID = TeamID;
+        this.Name = teamName;
+        this.members = members;
+    }
+
+    public String getTeamID() {
+        return TeamID;
+    }
+
+    public void setTeamID(String TeamID) {
+        this.TeamID = TeamID;
+    }
+
+   
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public ArrayList<Swimmers> getMembers() {
+        return members;
+    }
+
+    public void setMembers(ArrayList<Swimmers> members) {
+        this.members = members;
+    }
     
+    @Override
+    public String toString() {
+        return "Team{" + "teamID=" + TeamID + ", teamName=" + Name + ", users=" + members + '}';
+    }
+
+   
 }
