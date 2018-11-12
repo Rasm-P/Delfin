dff00f13e4dc751723dee523dba1132a49388904
Rasmus Hemmingsen <rashem2@hotmail.com>
2018-11-12 11:39:23 +0100

    com

--- Delfin/src/delfin/Competition.java
+++ Delfin/src/delfin/Competition.java
@@ -12,11 +12,25 @@ import java.util.ArrayList;
  * @author Dxtlxv Xndxrsxn
  */
 public class Competition {
-private String comName;
-private double time;
-private int placement;
-private String disciplin;
-private ArrayList<Result> placements;
 
-    
+    private String comName;
+    private double time;
+    private int placement;
+    private String disciplin;
+    private ArrayList<Result> placements;
+
+    public Competition(String comName, double time, int placement, String disciplin, ArrayList<Result> placements) throws IllegalAccessException
+    {
+        if (comName == null || time < 0 || placement < 0 || disciplin == null || placements.size() < 0)
+        {
+            throw new IllegalAccessException("Noob");
+        }
+
+        this.comName = comName;
+        this.time = time;
+        this.placement = placement;
+        this.disciplin = disciplin;
+        this.placements = placements;
+    }
+
 }
