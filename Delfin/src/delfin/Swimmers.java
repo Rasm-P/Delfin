04fae6e5ba00cfc6004178c0676729234f0cee8a
Rasmus Hemmingsen <rashem2@hotmail.com>
2018-11-12 10:45:12 +0100

    Swimmer class

--- Delfin/src/delfin/Swimmers.java
+++ Delfin/src/delfin/Swimmers.java
@@ -16,6 +16,68 @@ public class Swimmers {
     private String name;
     private Date dob;
     private String email;
+    private boolean status;
+    private boolean pro; 
+
+    public Swimmers(String name, Date dob, String email, boolean status, boolean pro)
+    {
+        this.name = name;
+        this.dob = dob;
+        this.email = email;
+        this.status = status;
+        this.pro = pro;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public Date getDob()
+    {
+        return dob;
+    }
+
+    public void setDob(Date dob)
+    {
+        this.dob = dob;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public boolean isStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(boolean status)
+    {
+        this.status = status;
+    }
+
+    public boolean isPro()
+    {
+        return pro;
+    }
+
+    public void setPro(boolean pro)
+    {
+        this.pro = pro;
+    }
+    
     
     
             
