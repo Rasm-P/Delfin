c0e832196e85e4992ac16cee9440acf0d2f1b051
Rasm-P <44894132+Rasm-P@users.noreply.github.com>
2018-11-13 11:47:02 +0100

    We have added a filereader and a sets main for the filereader to text document

--- Delfin/src/Data/Filereader.java
+++ Delfin/src/Data/Filereader.java
@@ -6,18 +6,47 @@
 package Data;
 
 import delfin.Swimmers;
+import java.awt.Point;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Rasmus2
  */
 public class Filereader {
+
     private static final boolean DEBUG = true;
-    String inFilename = "C:\\Users\\Rasmus2\\Desktop\\TestIO.txt";
-    String outFilename = "C:\\Users\\Rasmus2\\Desktop\\TestIO.txt";
+    String inFilename = "C:\\Users\\Rasmus2\\Documents\\GitHub\\Delfin\\Delfin\\delfiner.txt";
+    //String outFilename = "C:\\Users\\Rasmus2\\Documents\\GitHub\\Delfin\\Delfin\\delfiner.txt";
     
-    public ArrayList<Swimmers> getSvimmersByName() {
-        
+    public ArrayList<String[]> getSwimmersByName() {
+        try {
+            ArrayList<String[]> swimmers = new ArrayList();
+            String splitBy = ", ";
+
+            Charset cs = Charset.forName("UTF-8");
+            Path path = Paths.get(inFilename);
+            List<String> lines = Files.readAllLines(path, cs);
+            for (String line : lines) {
+                String[] swim = line.split(splitBy);
+                swimmers.add(swim);
+            }
+            return swimmers;
+        } catch (IOException ex) {
+            if (DEBUG) {
+                ex.printStackTrace();
+            }
+        }
+        return null;
     }
 }
