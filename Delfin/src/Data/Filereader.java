0adea89fa614cca2e34f21ae735e86127ff6099b
Rasm-P <44894132+Rasm-P@users.noreply.github.com>
2018-11-13 10:01:14 +0100

    New tekst file class instead of database file reader

---
+++ Delfin/src/Data/Filereader.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+import delfin.Swimmers;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Rasmus2
+ */
+public class Filereader {
+    private static final boolean DEBUG = true;
+    String inFilename = "C:\\Users\\Rasmus2\\Desktop\\TestIO.txt";
+    String outFilename = "C:\\Users\\Rasmus2\\Desktop\\TestIO.txt";
+    
+    public ArrayList<Swimmers> getSvimmersByName() {
+        
+    }
+}
